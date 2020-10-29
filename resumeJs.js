62d2d1fdf7506ee7e09246605cdffbb5692a41d2
Soufiane Izend <soufiane.izend@hotmail.com>
2020-10-29 14:57:31 +0100

    Adding animation to the showDetails

--- resumeJs.js
+++ resumeJs.js
@@ -6,12 +6,22 @@ function showDiv(variable) {
             child.style.display = "none";
         });
         x.style.display = "block";
+        animate({
+            duration: 1000,
+            timing: function(timeFraction) {
+                return timeFraction*50;
+            },
+            draw: function(progress) {
+                x.style.marginTop = -50 +(Math.floor(progress )) + 'px' ;
+            }
+        });
 
     } else {
         x.style.display = "none";
     }
 
 }
+
 window.addEventListener("load", function () {
     var couponcodes = document.getElementsByClassName("comp");
     for (var i = 0; i < couponcodes.length; i++) {
@@ -25,3 +35,34 @@ window.addEventListener("load", function () {
         });
     }
 });
+
+var tooltip = document.querySelectorAll('.coupontooltip');
+
+document.addEventListener('mousemove', fn, false);
+function fn(e) {
+    for (var i=tooltip.length; i--;) {
+        tooltip[i].style.left = e.pageX + 'px';
+        tooltip[i].style.top = e.pageY + 'px';
+    }
+}
+
+function animate({duration, draw, timing}) {
+
+    let start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        let progress = timing(timeFraction)
+
+        draw(progress);
+
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
+
+    });
+}
+
+
